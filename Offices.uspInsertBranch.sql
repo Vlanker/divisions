98d80e25c3509b7e6f4780e6371e5faf2b2f04f8
DESKTOP-RVJ25LH\vlank-h <kinvlanknik@yandex.ru>
2018-10-28 17:18:22 +0300

    Db: Delete [Structure], add FK_Workers_Departaments_DepartamentID

--- Offices.uspInsertBranch.sql
+++ Offices.uspInsertBranch.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROCEDURE [Offices].[uspInsertBranch]  
+CREATE PROCEDURE [Offices].[uspInsertBranch]  
 	@Title NVARCHAR (50), @DesedantID INT, @Level INT
 AS  
 BEGIN  
@@ -9,12 +9,8 @@ VALUES (@Title)
 
 SELECT @DepID = SCOPE_IDENTITY();  
 
-INSERT INTO [Offices].[Structure] ([AncestorID], [DescendarID],[Level])
-SELECT * FROM (SELECT S.[AncestorID], @DepID, S.[Level] + 1
-FROM [Structure] AS S
-WHERE S.[DescendarID] = @DesedantID
-UNION ALL
-SELECT @DepID, @DepID, @Level) as tmp
+INSERT INTO [Offices].[Structure] ([AncestorID], [DescendarID], [Level])
+(SELECT [S].[AncestorID],  @DepID, [S].[Level] + 1 FROM [Structure] AS [S] WHERE [S].[DescendarID] = @DesedantID UNION ALL SELECT @DepID, @DepID, @Level)
 
 COMMIT TRANSACTION   
 END
