3a2d9728cc1f9d6bf2d8b77453b41172db74c88a
Vlanker <kinvlanknik@yandex.ru>
2018-10-30 17:02:54 +0300

    commit db and LocalDbQuerisLabrary

--- Divisions/Divisions/FNewOrChangeWorker.cs
+++ Divisions/Divisions/FNewOrChangeWorker.cs
@@ -45,39 +45,39 @@ namespace Divisions
         {
             if (IsTitlesValid())
             {
-                using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.connString))
-                {
-                    const string sql = "UPDATE [Offices].[Workers] SET [StructureID] = @StructureID, [PersNum] = @PersNum, [FullName] = @Fullname, [Birthday] = @Birthday, [HiringDay] = @HiringDay,  [Salary] = @Salary, [ProfArea] = @ProfArea, [Gender] = @Gender WHERE [WorkerID] = @WorkerID";
+                //using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.connString))
+                //{
+                //    const string sql = "UPDATE [Offices].[Workers] SET [StructureID] = @StructureID, [PersNum] = @PersNum, [FullName] = @Fullname, [Birthday] = @Birthday, [HiringDay] = @HiringDay,  [Salary] = @Salary, [ProfArea] = @ProfArea, [Gender] = @Gender WHERE [WorkerID] = @WorkerID";
 
-                    using (SqlCommand sqlCommand = new SqlCommand(sql, connection))
-                    {
-                        sqlCommand.CommandType = CommandType.Text;
-                        //sqlCommand.Parameters.Add(new SqlParameter("@StructureID", SqlDbType.Int)).Value = FmMain.StructureID;
-                        sqlCommand.Parameters.Add(new SqlParameter("@PersNum", SqlDbType.NVarChar, 50)).Value = tbPersNum.Text;
-                        sqlCommand.Parameters.Add(new SqlParameter("@FullName", SqlDbType.NVarChar, 250)).Value = tbFullName.Text;
-                        sqlCommand.Parameters.Add(new SqlParameter("@Birthday", SqlDbType.Date)).Value = dtpBirthday.Value.ToShortDateString();
-                        sqlCommand.Parameters.Add(new SqlParameter("@HiringDay", SqlDbType.Date)).Value = dtpHiringDay.Value.ToShortDateString();
-                        sqlCommand.Parameters.Add(new SqlParameter("@Salary", SqlDbType.Money)).Value = GetSalary();
-                        sqlCommand.Parameters.Add(new SqlParameter("@ProfArea", SqlDbType.NVarChar, 150)).Value = tbProfArea.Text;
-                        sqlCommand.Parameters.Add(new SqlParameter("@Gender", SqlDbType.Bit)).Value = Convert.ToBoolean(cbGender.SelectedIndex);
-                        sqlCommand.Parameters.Add(new SqlParameter("@WorkerID", SqlDbType.Int)).Value = workerID;
+                //    using (SqlCommand sqlCommand = new SqlCommand(sql, connection))
+                //    {
+                //        sqlCommand.CommandType = CommandType.Text;
+                //        //sqlCommand.Parameters.Add(new SqlParameter("@StructureID", SqlDbType.Int)).Value = FmMain.StructureID;
+                //        sqlCommand.Parameters.Add(new SqlParameter("@PersNum", SqlDbType.NVarChar, 50)).Value = tbPersNum.Text;
+                //        sqlCommand.Parameters.Add(new SqlParameter("@FullName", SqlDbType.NVarChar, 250)).Value = tbFullName.Text;
+                //        sqlCommand.Parameters.Add(new SqlParameter("@Birthday", SqlDbType.Date)).Value = dtpBirthday.Value.ToShortDateString();
+                //        sqlCommand.Parameters.Add(new SqlParameter("@HiringDay", SqlDbType.Date)).Value = dtpHiringDay.Value.ToShortDateString();
+                //        sqlCommand.Parameters.Add(new SqlParameter("@Salary", SqlDbType.Money)).Value = GetSalary();
+                //        sqlCommand.Parameters.Add(new SqlParameter("@ProfArea", SqlDbType.NVarChar, 150)).Value = tbProfArea.Text;
+                //        sqlCommand.Parameters.Add(new SqlParameter("@Gender", SqlDbType.Bit)).Value = Convert.ToBoolean(cbGender.SelectedIndex);
+                //        sqlCommand.Parameters.Add(new SqlParameter("@WorkerID", SqlDbType.Int)).Value = workerID;
 
-                        try
-                        {
-                            connection.Open();
-                            sqlCommand.ExecuteNonQuery();
-                        }
-                        catch
-                        {
-                            MessageBox.Show("Работник не изменён");
-                        }
-                        finally
-                        {
-                            connection.Close();
-                        }
-                    }
+                //        try
+                //        {
+                //            connection.Open();
+                //            sqlCommand.ExecuteNonQuery();
+                //        }
+                //        catch
+                //        {
+                //            MessageBox.Show("Работник не изменён");
+                //        }
+                //        finally
+                //        {
+                //            connection.Close();
+                //        }
+                //    }
                     
-                }
+                //}
                 this.Close();
             }
         }
@@ -103,7 +103,7 @@ namespace Divisions
                     {
                         sqlCommand.CommandType = CommandType.Text;
 
-                        //sqlCommand.Parameters.Add(new SqlParameter("@StructureID", SqlDbType.Int)).Value = FmMain.StructureID;
+                        wsqlCommand.Parameters.Add(new SqlParameter("@StructureID", SqlDbType.Int)).Value = FmMain.StructureID;
                         sqlCommand.Parameters.Add(new SqlParameter("@PersNum", SqlDbType.NVarChar, 50)).Value = tbPersNum.Text;
                         sqlCommand.Parameters.Add(new SqlParameter("@FullName", SqlDbType.NVarChar, 250)).Value = tbFullName.Text;
                         sqlCommand.Parameters.Add(new SqlParameter("@Birthday", SqlDbType.Date)).Value = dtpBirthday.Value.ToShortDateString();
