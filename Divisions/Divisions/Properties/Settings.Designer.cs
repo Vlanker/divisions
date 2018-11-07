aa19efc1e6c333e238b07ed8cad389ce4704ff11
Vlanker <kinvlanknik@yandex.ru>
2018-11-07 11:32:51 +0300

    fix

--- Divisions/Divisions/Properties/Settings.Designer.cs
+++ Divisions/Divisions/Properties/Settings.Designer.cs
@@ -22,16 +22,5 @@ namespace Divisions.Properties {
                 return defaultInstance;
             }
         }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDb)\\MSSQLLocalDb;Initial Catalog=Divisions;Integrated Security=" +
-            "True;Pooling=True")]
-        public string connString {
-            get {
-                return ((string)(this["connString"]));
-            }
-        }
     }
 }
